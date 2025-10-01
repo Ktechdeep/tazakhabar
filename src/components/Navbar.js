56ad7fc2595f364d456528e71dceab931bdc3fad
kuldeep <peedluek@gmail.com>
2025-10-01 17:27:31 +0530

    Added Routes for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
-export class navbar extends Component {
+export class Navbar extends Component {
   render() {
     return (
       <div>
-
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">TazaKhabar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <Link className="navbar-brand" to="/">TazaKhabar</Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon"></span>
             </button>
+            
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/about">About</a>
+                  <Link className="nav-link" to="/entertainment">Entertainment</Link>
                 </li>
-                <li className="nav-item"><a className="nav-link active" aria-current="page" href="/business">business</a></li>
-                <li className="nav-item"><a className="nav-link active" aria-current="page" href="/entertainment">entertainment</a> </li>
-                <li className="nav-item"><a className="nav-link active" aria-current="page" href="/general">general</a> </li>
-                <li className="nav-item"><a className="nav-link active" aria-current="page" href="/health">health</a></li>
-                <li className="nav-item"><a className="nav-link active" aria-current="page" href="/science">science</a> </li>
-                <li className="nav-item"><a className="nav-link active" aria-current="page" href="/sports">sports</a></li>
-                <li className="nav-item"><a className="nav-link active" aria-current="page" href="/technology">technology</a></li>
-
-
+                <li className="nav-item">
+                  <Link className="nav-link" to="/business">Business</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/health">Health</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/science">Science</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/sports">Sports</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/technology">Technology</Link>
+                </li>
               </ul>
             </div>
           </div>
         </nav>
-
-
-
-
-
       </div>
-    )
+    );
   }
 }
 
-export default navbar
+export default Navbar;
